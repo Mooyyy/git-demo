33e971ae1b092879fddb26175dc39ea284c87b0b
Moy <molinareymon@rocketmail.com>
2021-05-24 14:14:59 -0700

    fix error in hello.java

--- src/hello.java
+++ src/hello.java
@@ -1,3 +1,6 @@
-public static void main(String args[]){
-    System.out.println("Hello world");
+class hello{
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Hello world");
+    }
 }
